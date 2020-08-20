b7344df2c2fd9efd19150b6f8391fc90dda8e020
Sebastien Langlois <42305216+sebastienlanglois@users.noreply.github.com>
2020-08-20 19:12:58 -0400

    Update main.js

--- _static/js/main.js
+++ _static/js/main.js
@@ -1,5 +1,13 @@
-/*
+code_show=true; 
+function code_toggle() {
+if (code_show){
+$('div.input').hide();
+value='Display code'
+} else {
+$('div.input').not(':first').show();
+}
+code_show = !code_show
+} 
+$( document ).ready(code_toggle);
 
-nothing to see here
-
-*/
+$('div.cell.code_cell.rendered.selected').find('div.input').hide()
