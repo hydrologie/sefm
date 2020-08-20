2a5212066147b55b2df3f6f2e81bf624f57570b6
sebastienlanglois <42305216+sebastienlanglois@users.noreply.github.com>
2020-08-20 23:29:01 +0000

    Deploying to gh-pages from  @ f0105b5f7735887f30707467539dd3403d97ce5e 🚀

--- _static/js/main.js
+++ _static/js/main.js
@@ -1,13 +1,5 @@
-code_show=true; 
-function code_toggle() {
-if (code_show){
-$('div.input').hide();
-value='Display code'
-} else {
-$('div.input').not(':first').show();
-}
-code_show = !code_show
-} 
-$( document ).ready(code_toggle);
+/*
 
-$('div.cell.code_cell.rendered.selected').find('div.input').hide()
+nothing to see here
+
+*/
